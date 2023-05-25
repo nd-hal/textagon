8755dc5e8ae076c9b3d5279f72ebd6e22b0421ce
Ruiyang Qin <35632188+ruiyangqin2016@users.noreply.github.com>
2023-05-25 10:59:17 -0400

    Add files via upload

--- setup.py
+++ setup.py
@@ -9,26 +9,27 @@ with open(readme_path, "r", encoding="utf-8") as fh:
 
 setup(
     name = 'textagon',
-    version = '0.1.17',
+    version = '0.1.18',
     packages = find_packages(),
     description = 'Textagon is a powerful tool for text data analysis, providing a means to visualize parallel representations of your data and gain insight into the impact of various lexicons on two classes of text data.',
     long_description=long_description,
     long_description_content_type='text/markdown', 
-    author = 'Ruiyang Qin',
-    author_email = 'rqin@nd.edu',
+    author = 'Ahmed Abbasi, David Dobolyi, John Lalor, Ruiyang Qin',
+    author_email = 'aabbasi@nd.edu, david.dobolyi@colorado.edu,  john.lalor@nd.edu, rqin@nd.edu',
     classifiers=[
         "License :: OSI Approved :: Python Software Foundation License"
     ],
+    python_requires='>=3.6',
     install_requires=[
-        'nltk',
-        'scikit-learn',
-        'pandas',
-        'numpy',
-        'beautifulsoup4',
-        'mapply',
-        'pyenchant',
-        'tzlocal',
-        'spacy',
+        'nltk >= 3.8.1',
+        'scikit-learn >= 1.2.1',
+        'pandas >= 1.5.3',
+        'numpy >= 1.24.2',
+        'beautifulsoup4 >= 4.11.2',
+        'mapply >= 0.1.21',
+        'pyenchant >= 3.2.2',
+        'tzlocal >= 4.3',
+        'spacy >= 3.5.2',
     ],
     entry_points={
         'console_scripts': [
