35dc1544f7ff67ff233417f68302415321791e19
John Lalor <john.lalor@nd.edu>
2023-08-21 10:54:16 -0400

    Add files via upload

--- textagon/external/extracted/WNAffect-master/wnaffect.py
+++ textagon/external/extracted/WNAffect-master/wnaffect.py
@@ -8,17 +8,19 @@ import sys
 import nltk
 from emotion import Emotion
 from nltk.corpus import WordNetCorpusReader
-from nltk.corpus import wordnet as wn
 import xml.etree.ElementTree as ET
 import pkg_resources
 
 class WNAffect:
     """WordNet-Affect ressource."""
     
-    def __init__(self, wordnet_dir, wn_domains_dir):
+    def __init__(self, wordnet16_dir, wn_domains_dir):
         """Initializes the WordNet-Affect object."""
         
-        self.wn16 = wn
+        cwd = os.getcwd()
+        nltk.data.path.append(cwd)
+        wn16_path = "{0}/dict".format(wordnet16_dir)
+        self.wn16 = WordNetCorpusReader(wn16_path, omw_reader = None)
         self.flat_pos = {'NN':'NN', 'NNS':'NN', 'JJ':'JJ', 'JJR':'JJ', 'JJS':'JJ', 'RB':'RB', 'RBR':'RB', 'RBS':'RB', 'VB':'VB', 'VBD':'VB', 'VGB':'VB', 'VBN':'VB', 'VBP':'VB', 'VBZ':'VB'}
         self.wn_pos = {'NN':self.wn16.NOUN, 'JJ':self.wn16.ADJ, 'VB':self.wn16.VERB, 'RB':self.wn16.ADV}
         self._load_emotions(wn_domains_dir)
@@ -29,11 +31,10 @@ class WNAffect:
     def _load_synsets(self, wn_domains_dir):
         """Returns a dictionary POS tag -> synset offset -> emotion (str -> int -> str)."""
         
-        a_synsets_xml_path = pkg_resources.resource_filename('textagon', 'external/extracted/wn-domains/wn-affect-1.1/a-synsets.xml')
-        tree = ET.parse(a_synsets_xml_path)
+        tree = ET.parse("{0}/wn-affect-1.1/a-synsets.xml".format(wn_domains_dir))
         root = tree.getroot()
         pos_map = { "noun": "NN", "adj": "JJ", "verb": "VB", "adv": "RB" }
-
+    
         synsets = {}
         for pos in ["noun", "adj", "verb", "adv"]:
             tag = pos_map[pos]
@@ -45,14 +46,13 @@ class WNAffect:
                     synsets[tag][offset] = Emotion.emotions[elem.get("categ")] if elem.get("categ") in Emotion.emotions else None
                 elif elem.get("noun-id"):
                     synsets[tag][offset] = synsets[pos_map["noun"]][int(elem.get("noun-id")[2:])]
-        
+    
         return synsets
         
     def _load_emotions(self, wn_domains_dir):
         """Loads the hierarchy of emotions from the WordNet-Affect xml."""
         
-        a_hierarchy_xml_path = pkg_resources.resource_filename('textagon', 'external/extracted/wn-domains/wn-affect-1.1/a-hierarchy.xml')
-        tree = ET.parse(a_hierarchy_xml_path)
+        tree = ET.parse("{0}/wn-affect-1.1/a-hierarchy.xml".format(wn_domains_dir))
         root = tree.getroot()
         for elem in root.findall("categ"):
             name = elem.get("name")
